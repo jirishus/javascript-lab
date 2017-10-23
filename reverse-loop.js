0ae906d1143864d580c293c885d550d83630dd9f
Brandon Gagon <jirishus@yahoo.com>
2017-10-23 10:24:30 -0700

    update to reverse-loop.js

--- reverse-loop.js
+++ reverse-loop.js
@@ -1,1 +1,12 @@
+var str = 'samplestring';
 
+function reverseString(s) {
+  var len = s.length;
+  var str = '';
+  for (let i=len - 1;i>=0;i--) {
+   str += s[i];
+  }
+  return str;
+}
+
+var result = reverseString(str);
