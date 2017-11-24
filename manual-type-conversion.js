0c3c64d4fbe1b3e32d34815a99f64ee8966529a3
Brandon Gagon <jirishus@yahoo.com>
2017-11-24 11:36:10 -0500

    Ability to handle floating point numbers

--- manual-type-conversion.js
+++ manual-type-conversion.js
@@ -1,21 +1,32 @@
 // Convert string to integer 
 // without casting or parseInt()
 
-var sample = '-43002';
+var sample = '-43002.22';
 
 function convert(str) {
   var resultInt = 0;
   var factor = 1;
   var radix = 10;
+  var clean = '';
   var isSigned = false;
   
+  // check if signed or unsigned integer
   if (str[0] === '-') {
     isSigned = true;
     str = str.replace('-','');
   }
   
-  for (var i=str.length - 1;i>=0;i--) {
-  	resultInt += str[i] * factor;
+  // check if floating point
+  for (var j=0;j<str.length;j++) {
+   if (str[j] === '.') {
+		break;
+   } else {
+    clean += str[j];
+   }
+  }
+     
+  for (var i=clean.length - 1;i>=0;i--) {
+  	resultInt += clean[i] * factor;
     factor *= radix;
   }
 	
