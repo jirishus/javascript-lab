acee14dca67da3a1589a1fbdb1e554593c5aca04
Brandon Gagon <jirishus@yahoo.com>
2017-11-24 10:10:19 -0500

    Ability to handle signed integers

--- manual-type-conversion.js
+++ manual-type-conversion.js
@@ -1,20 +1,29 @@
 // Convert string to integer 
 // without casting or parseInt()
 
-var sample = '43002';
+var sample = '-43002';
 
 function convert(str) {
-	var resultInt = 0;
+  var resultInt = 0;
   var factor = 1;
+  var radix = 10;
+  var isSigned = false;
+  
+  if (str[0] === '-') {
+    isSigned = true;
+    str = str.replace('-','');
+  }
   
   for (var i=str.length - 1;i>=0;i--) {
   	resultInt += str[i] * factor;
-    factor *= 10;
+    factor *= radix;
   }
-  
+	
+  if (isSigned) {
+   resultInt = 0 - resultInt;
+  }
   return resultInt;
 }
 
-var pl = convert(sample);
-
-console.log(pl);
+var payload = convert(sample);
+console.log(payload);
