4dc96fc1c3cf8c3da4bb5cb1c2b24f182627f1f0
jirishus <jirishus@yahoo.com>
2018-01-07 21:40:14 -0800

    stack push method

--- stack.js
+++ stack.js
@@ -1,6 +1,6 @@
 function Stack() {
  this.size = 0;
- this.top = null;
+ this.head = null;
 }
 
 function Node(data) {
@@ -8,7 +8,25 @@ function Node(data) {
  this.next = null;
 }
 
+Stack.prototype.push = function(value) {
+ var node = new Node(value);
+ // first node
+ if (!this.head) {
+  this.head = node;
+ } else {
+  // set new node next to the original head node
+  node.next = this.head;
+  // set head to newly inserted node
+  this.head = node;
+ }
+ // increment size on each insertion
+ this.size++;
+}
+
 // mock browser history using Stack
 var browser = new Stack();
+// push data our browser stack
+browser.push('www.google.com');
+browser.push('www.learnsteady.com');
 
 console.log(browser);
