6d50c003ee75cb726f3c5175936673a989753a2d
Brandon Gagon <jirishus@yahoo.com>
2018-05-15 23:59:54 -0700

    proper function documentation

--- linked_list2.js
+++ linked_list2.js
@@ -1,20 +1,36 @@
-
-// List Object for Nodes of Data
+/**
+ * Represents a LinkedList
+ * @constructor
+ */
 function LinkedList() {
  this.head = null;
 }
 
-// Basic Node Object
+/**
+ * Represents a Node
+ * @constructor
+ * @param {object} data - The data to be stored in Node.
+ */
 function Node(data) {
  this.data = data;
  this.next = null;
 }
 
-// Method to add Nodes to LinkedList
-LinkedList.prototype.add = function() {}
+/**
+ * Method to add Nodes to LinkedList
+ * @prototype
+ * @param {Node} - The new Node to be inserted into the LinkedList
+ */
+LinkedList.prototype.add = function(node) {
+ // first check if linkedList head is null, if so add first node.
+ return 'Not Implemented';
+}
 
-// Create New LinkedList Object
+// Create inkedList Object
 let list = new LinkedList();
 
-// Create New Node Object
+// Create Node Object
 let node10 = new Node('mockdata');
+
+// Add Node Object to LinkedList
+list.add(node10);
