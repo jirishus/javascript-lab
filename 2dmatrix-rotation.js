52765e4ed0bcc4f8413cf1cb70530ed67178215b
jirishus <jirishus@yahoo.com>
2018-08-06 21:48:26 -0700

    rotate matrix and palindome file extension

--- 2dmatrix-rotation.js
+++ 2dmatrix-rotation.js
@@ -6,4 +6,41 @@
 *   [3,4]
 *  ]
 *
+* result
+* [
+*  [3,1],
+*  [4,2]
+* ]
 */
+
+var matrix =
+[
+   [1,2],
+   [3,4]
+];
+
+/**
+ * Define Our Function
+ */
+function rotateClockWise(matrix) {
+ // 1. flip matrix
+ matrix.reverse();
+ // 2. iterate and swap the corners
+ for (var i = 0; i < matrix.length; i++) {
+  for (var j = 0; j < i; j++) {
+   const temp = matrix[i][j];
+   matrix[i][j] = matrix[j][i];
+   matrix[j][i] = temp;
+  }
+ }
+
+ return matrix;
+}
+
+let result = rotateClockWise(matrix);
+
+/**
+ * Log Output
+ */
+
+ console.log('Result : ', result);
