9d8edbf2230dfb5083e601ca8a08f92ec38c1e76
jirishus <jirishus@yahoo.com>
2018-09-30 18:26:26 -0700

    setup for subsequence steps

--- isSubsequence.js
+++ isSubsequence.js
@@ -7,7 +7,17 @@
  * User Functions
  */
 
- function isSub(str1, str2) {}
+ function isSub(str1, str2) {
+  /**
+   * General runtime O(n) where n is
+   * length of string2
+   *
+   * Iterative solution will iterate both strings
+   */
+   let len1 = str1.length;
+   let len2 = str2.length;
+
+ }
 
 
 /**
