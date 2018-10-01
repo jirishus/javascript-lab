77af00c5f4a5885101bfad17afd7ecb76c81e7ed
jirishus <jirishus@yahoo.com>
2018-09-30 18:10:12 -0700

    subsequence algorithm

---
+++ isSubsequence.js
@@ -0,0 +1,20 @@
+
+/**
+ * Load Modules
+ */
+
+/**
+ * User Functions
+ */
+
+ function isSub(str1, str2) {}
+
+
+/**
+ * Driver Code
+ */
+
+ let str1 = 'able';
+ let str2 = 'appppble';
+
+ isSub(str1, str2);
