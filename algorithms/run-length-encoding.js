d8cdca049beef33c2f413d8bc51ef4f6fecb9bb4
jirishus <jirishus@yahoo.com>
2025-03-26 22:34:20 -0700

    run length encoding

---
+++ algorithms/run-length-encoding.js
@@ -0,0 +1,23 @@
+/**
+ * 
+ * @param {string} str 
+ * @returns string
+ */
+function encode(str) {
+  if (!str) return '';
+
+  let output = '';
+  let count = 1;
+
+  for (let i=1;i<str.length;i++) {
+    if (i + 1 < str.length && str[i] === str[i + 1]) {
+      count++;
+    } else {
+      output += count + str[i];
+      count = 1;
+    }
+  }
+  return output;
+}
+
+console.log(encode('aaaaab'));
