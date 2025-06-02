cd1234ab6e096e0e4668c8ad2fa4bbb578b590d3
jirishus <jirishus@yahoo.com>
2025-06-01 18:44:40 -0700

    added slidingWindow folder and longest substring algo

---
+++ algorithms/slidingWindow/lengthOfLongestSubstring.js
@@ -0,0 +1,23 @@
+/*
+Given a string s, find the length of the longest substring without duplicate characters
+*/
+function lengthOfLongestSubstring(s) {
+  let set = new Set();
+  let left = 0;
+  let maxSize = 0;
+
+  if (s.length === 0) return 0;
+  if (s.length === 1) return 1;
+
+  for (let i=0;i<s.length;i++) {    
+    let curr = s[i];
+    while (set.has(curr)) {
+      set.delete(s[left]);
+      left++;
+    }
+
+    set.add(curr);
+    maxSize = Math.max(maxSize, i - left + 1);
+  }
+  return maxSize;
+}
