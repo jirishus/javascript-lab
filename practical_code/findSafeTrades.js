b97bd5c386139aaa82573fac3718edbb7c736d61
jirishus <jirishus@yahoo.com>
2025-06-14 20:07:52 -0700

    find safe trades

---
+++ practical_code/findSafeTrades.js
@@ -0,0 +1,30 @@
+const trades = [
+  { symbol: 'AAPL', date: '2025-06-13' },
+  { symbol: 'TSLA', date: '2025-06-14' },
+  { symbol: 'NVDA', date: '2025-06-15' },
+  { symbol: 'TSLA', date: '2025-06-15' }
+];
+
+const alerts = [
+  { symbol: 'TSLA', date: '2025-06-14' }, // ⚠️ alert on the same day as one suggestion and day before another
+  { symbol: 'NVDA', date: '2025-06-14' }
+];
+
+function findSafeTrades(trades, alerts) {
+  const safeTrades = trades.filter((trade) => {
+    const isRisky = alerts.some((alert) => {
+      // if tickers don't match, proceed to next stock ticker
+      if (alert.symbol !== trade.symbol) return false;
+
+      const tradeDate = new Date(trade.date);
+      const alertDate = new Date(alert.date);
+      const diffDays = (tradeDate - alertDate) / (1000 * 60 * 60 * 24);
+
+      return diffDays === 0 || diffDays === 1;
+    });
+    // we negate and return the safe trades
+    return !isRisky;
+  });
+}
+
+
