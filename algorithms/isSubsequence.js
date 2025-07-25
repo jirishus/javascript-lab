b35a5d48366b1e2a27c1fba193976070fd62ca8b
jirishus <jirishus@yahoo.com>
2025-07-25 12:14:18 -0700

    isSubsequence using two pointers

--- algorithms/isSubsequence.js
+++ algorithms/isSubsequence.js
@@ -1,30 +1,21 @@
-
-/**
- * Load Modules
- */
-
-/**
- * User Functions
- */
-
- function isSub(str1, str2) {
-  /**
-   * General runtime O(n) where n is
-   * length of string2
-   *
-   * Iterative solution will iterate both strings
-   */
-   let len1 = str1.length;
-   let len2 = str2.length;
-
- }
-
+function isSub(s, t) {
+  let i=0,j=0;
+  while (i < s.length && j < t.length) {
+    if (s[i] === t[j]) {
+      i++;
+    }
+    j++;
+  }
+  return i === s.length;
+}
 
 /**
  * Driver Code
  */
 
- let str1 = 'able';
- let str2 = 'appppble';
+let str1 = 'able';
+let str2 = 'appppble';
 
- isSub(str1, str2);
+const r = isSub(str1, str2);
+
+console.log(r);
