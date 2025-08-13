179c0518c4070712e0c7bedc72845e0cd012e4d3
jirishus <jirishus@yahoo.com>
2025-08-12 22:24:54 -0700

    linkedlist folder and draft LRU cache

---
+++ algorithms/linkedLists/LRUCahce.js
@@ -0,0 +1,19 @@
+class Node {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class LRUCache {
+  constructor(capacity) {
+    this.capacity = capacity;
+    this.dic = new Map();
+    this.head = new Node(-1, -1);
+    this.tail = new Node(-1, -1);
+    this.head.next = this.tail;
+    this.tail.prev = this.head;
+  }
+}
